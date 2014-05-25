ed8db67d6eb94118b8303b36ef438a0b1e6cc3fa
Vypor <booker@3ky.cc>
2014-05-24 22:14:27 -0700

    Create DnsListener.pl

---
+++ DnsListener.pl
@@ -0,0 +1,36 @@
+use strict;
+use warnings;
+use Net::Pcap::Easy;
+
+if ( $#ARGV != 2 ) {
+    print "Usage: DnsListener.pl <interface> <maxbytesize> <outputfile>\n";
+    print "Example: perl DnsListener.pl eth1 3000\n";
+    print "Coded by Vypor, https://github.com/Vypor\n";
+    exit(1);
+}
+
+my $interface = $ARGV[0];
+my $leastsize = $ARGV[1];
+my $LOGFILE   = $ARGV[2];
+my $ethip = `/sbin/ifconfig $interface | grep "inet addr" | awk -F: '{print \$2}' | awk '{print \$1}'`;
+$ethip = substr( $ethip, 0, -1 );
+
+my $listener = Net::Pcap::Easy->new(
+    dev    => $interface,
+    filter => "port 53 and udp and not src host $ethip and greater $leastsize",
+    packets_per_loop => 10,
+    bytes_to_capture => 0,
+    timeout_in_ms    => 0,    # 0ms means forever
+    promiscuous      => 0,    # true or false
+
+    udp_callback => sub {
+        my ( $listener, $ether, $ip, $udp, $header ) = @_;
+
+        open( FILE, ">>$LOGFILE" );
+        print FILE "$ip->{src_ip} $udp->{len}\n";
+        close FILE;
+        print "$ip->{src_ip} $udp->{len}\n";
+
+    }
+);
+1 while $listener->loop;
